c4892a6a157c19874a42a55088186c5014d17a46
VanyEly <99744796+VanyEly@users.noreply.github.com>
2022-08-18 15:11:42 -0300

    creacion del BackEnd
    creacion del BackEnd y contenido en carpeta

---
+++ PI_BaldovinoVaninaE/BackEnd/src/main/java/com/portfolio/bve/BveApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.bve;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BveApplication.class, args);
+	}
+
+}
