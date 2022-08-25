2c26e051be265d39975b1f5fc82abf01949f2d76
VanyEly <99744796+VanyEly@users.noreply.github.com>
2022-08-25 14:58:33 -0300

    ceacion de packs
    creacion de los paquetes

---
+++ PI_BaldovinoVaninaE/BackEnd/src/main/java/com/portfolio/bve/Service/ImpPersonaService.java
@@ -0,0 +1,35 @@
+package com.portfolio.bve.Service;
+
+import com.portfolio.bve.Entity.Persona;
+import com.portfolio.bve.Interface.IPersonaService;
+import com.portfolio.bve.Repository.IPersonaRepository;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ImpPersonaService implements IPersonaService{
+@Autowired IPersonaRepository ipersonaRepository;
+
+            
+    @Override
+    public List<Persona> getPersona() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void savePersona(Persona persona) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void deletePersona(Long id) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Persona findPersona(Long id) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
