dd983773019e16668bc3b4362e7e4d00308f6ea4
VanyEly <99744796+VanyEly@users.noreply.github.com>
2022-08-30 18:18:55 -0300

    creacion de componentes
    creacion de componentes model y login

--- PI_BaldovinoVaninaE/BackEnd/src/main/java/com/portfolio/bve/Controller/PersonaController.java
+++ PI_BaldovinoVaninaE/BackEnd/src/main/java/com/portfolio/bve/Controller/PersonaController.java
@@ -5,6 +5,7 @@ import com.portfolio.bve.Entity.Persona;
 import com.portfolio.bve.Interface.IPersonaService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
   public class PersonaController {
     @Autowired IPersonaService ipersonaService;
     
@@ -49,5 +51,5 @@ import org.springframework.web.bind.annotation.RestController;
          ipersonaService.savePersona(persona);
          return persona;
     }
-    
-}
+   
+    }
