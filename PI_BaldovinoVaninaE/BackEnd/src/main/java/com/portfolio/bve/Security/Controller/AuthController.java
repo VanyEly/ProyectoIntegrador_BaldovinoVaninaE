ec3963c35656335622119cb5f885178657c2f6a5
VanyEly <99744796+VanyEly@users.noreply.github.com>
2022-10-08 10:29:40 -0300

    copiar todo
    terminado el back end

---
+++ PI_BaldovinoVaninaE/BackEnd/src/main/java/com/portfolio/bve/Security/Controller/AuthController.java
@@ -0,0 +1,62 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.portfolio.bve.Security.Controller;
+
+import com.portfolio.bve.Security.Dto.JwtDto;
+import com.portfolio.bve.Security.Dto.LoginUsuario;
+import com.portfolio.bve.Security.Service.RolService;
+import com.portfolio.bve.Security.Service.UsuarioService;
+import com.portfolio.bve.Security.jwt.JwtProvider;
+import javax.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+@CrossOrigin
+public class AuthController {
+    @Autowired
+    PasswordEncoder passwordEncoder;
+    @Autowired
+    AuthenticationManager authenticationManager;
+    @Autowired
+    UsuarioService usuarioService;
+    @Autowired
+    RolService rolService;
+    @Autowired
+    JwtProvider jwtProvider;
+    
+    @PostMapping("/login")
+    public ResponseEntity<JwtDto> login(@Valid @RequestBody LoginUsuario loginUsuario, BindingResult bindingResult){
+      if(bindingResult.hasErrors())
+          return new ResponseEntity(new Mensaje("Campos mal puestos"), HttpStatus.BAD_REQUEST);
+      
+      Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(loginUsuario.getNombreUsuario(),loginUsuario.getPassword()));
+      
+      SecurityContextHolder.getContext().setAuthentication(authentication);
+      
+      String jwt = jwtProvider.generateToken(authentication);
+      
+      UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+      
+      JwtDto jwtDto = new JwtDto(jwt, userDetails.getUsername(), userDetails.getAuthorities());
+      
+      return new ResponseEntity(jwtDto, HttpStatus.OK);
+    }
+    
+}
