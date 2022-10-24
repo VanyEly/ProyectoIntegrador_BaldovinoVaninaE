076851b934a36eaab330698baeee836be50d8314
VanyEly <99744796+VanyEly@users.noreply.github.com>
2022-10-23 22:16:27 -0300

    commit all
    commit all

---
+++ PI_BaldovinoVaninaE/BackEnd/src/main/java/com/portfolio/bve/Service/SExperiencia.java
@@ -0,0 +1,56 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.portfolio.bve.Service;
+
+import com.portfolio.bve.Entity.Experiencia;
+import com.portfolio.bve.Repository.RExperiencia;
+import java.util.List;
+import java.util.Optional;
+import javax.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+@Transactional
+public class SExperiencia {
+    @Autowired
+    RExperiencia rExperiencia;
+    
+    public List<Experiencia>list(){
+        return rExperiencia.findAll();
+    }
+    
+    public Optional<Experiencia> getOn(int id) {
+        return rExperiencia.findById(id);
+    }
+    
+    public Optional<Experiencia> getByNombreE(String nombreE){
+        return rExperiencia.findByNombreE(nombreE);
+    }
+    
+    public void save(Experiencia expe){
+        rExperiencia.save(expe);
+    }
+    
+    public void delete(int id){
+        rExperiencia.deleteById(id);
+    }
+    
+    public boolean existsById(int id){
+        return rExperiencia.existsById(id);
+    }
+    
+    public boolean exitsByNombreE(String nombreE){
+        return rExperiencia.existsByNombreE(nombreE);
+    }
+
+    public boolean existsByNombreE(String nombreE) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    public Object getOne(int id) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+}
