ae823799820269dfdd6113da18dd635f87d1bbe0
VanyEly <99744796+VanyEly@users.noreply.github.com>
2022-10-27 10:29:06 -0300

    commit all
    commit all

--- PI_BaldovinoVaninaE/FrontEnd/src/app/components/hys/edit-skill.component.ts
+++ PI_BaldovinoVaninaE/FrontEnd/src/app/components/hys/edit-skill.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Skill } from 'src/app/model/skill';
+import { SkillService } from 'src/app/service/skill.service';
 
 @Component({
   selector: 'app-edit-skill',
@@ -6,10 +9,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit-skill.component.css']
 })
 export class EditSkillComponent implements OnInit {
+  skill: Skill = null;
 
-  constructor() { }
+  constructor(
+    private skillS: SkillService,
+    private activatedRouter: ActivatedRoute,
+    private router: Router) { }
 
   ngOnInit(): void {
+    const id = this.activatedRouter.snapshot.params['id'];
+    this.skillS.detail(id).subscribe(
+      data => {
+        this.skill = data;
+      }, err => {
+        alert("Error al modificar");
+        this.router.navigate(['']);
+      }
+    )
   }
-
+  
+  onUpdate(){
+    const id = this.activatedRouter.snapshot.params['id'];
+    this.skillS.update(id, this.skill).subscribe(
+      data => {
+        this.router.navigate(['']);
+      }, err => {
+        alert("Error al modificar la skill");
+        this.router.navigate(['']);
+      }
+    )
+  }
 }
