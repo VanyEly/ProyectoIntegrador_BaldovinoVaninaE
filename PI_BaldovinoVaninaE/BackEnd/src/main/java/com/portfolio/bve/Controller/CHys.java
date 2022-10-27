76614a02520d03234e71a4efae4e4731207b5057
VanyEly <99744796+VanyEly@users.noreply.github.com>
2022-10-27 12:06:01 -0300

    commit all
    commit all

--- PI_BaldovinoVaninaE/BackEnd/src/main/java/com/portfolio/bve/Controller/CHys.java
+++ PI_BaldovinoVaninaE/BackEnd/src/main/java/com/portfolio/bve/Controller/CHys.java
@@ -24,9 +24,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = "http//localhost:4200")
-//@CrossOrigin(origins = "https://frontedbal.web.app")
-@RequestMapping("/hys")
+//@CrossOrigin(origins = "http//localhost:4200")
+@CrossOrigin(origins = "https://frontedbal.web.app")
+@RequestMapping("/skill")
 public class CHys {
      @Autowired
     Shys shys;
